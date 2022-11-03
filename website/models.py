3825533e5283ba6088e6a69927a59cf5979e9297
GigaScott <scott.gamit.09@outlook.com>
2022-11-03 14:32:46 -0400

    first commit

---
+++ website/models.py
@@ -0,0 +1,22 @@
+from email.policy import default
+from enum import unique
+from time import timezone
+from . import db 
+from flask_login import UserMixin
+from sqlalchemy.sql import func
+
+class Clothing(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    description = db.Column(db.String(10000))
+    flaws = db.Column(db.String(10000))
+    size = db.Column(db.String(10000))
+    measurements = db.Column(db.String(10000))
+    date = db.Column(db.DateTime(timezone=True), default=func.now())
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id')) #one to many relationship
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(150), unique=True)
+    password = db.Column(db.String(150))
+    first_name = db.Column(db.String(150))
+    notes = db.relationship('Note')
---
+++ website/static/index.js
@@ -0,0 +1,8 @@
+function deleteNote(noteId){
+    fetch('/delete-note', {
+        method: 'POST',
+        body: JSON.stringify({noteID: noteId}),
+    }).then((_res) => {
+        window.location.href ='/'
+    })
+}
